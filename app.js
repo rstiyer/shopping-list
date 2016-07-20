e0c94024d3722fc3b3db3282d4222d8633623417
rstiyer <rachel.stiyer@gmail.com>
2016-07-20 13:52:09 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('ul').on('click', '.shopping-item-toggle', function() {
+		$(this.closest('.shopping-item-controls')).prev().toggleClass('shopping-item__checked');
+	})
+	.on('click', '.shopping-item-delete', function() {
+		$(this.closest('li')).remove();
+	});
+
+	$('#js-shopping-list-form').submit(function() {
+		event.preventDefault();
+		$('li:last-child').clone().insertAfter('li:last-child');
+      	$('li:last-child .shopping-item').text($('#shopping-list-entry').val());
+	});
+});
